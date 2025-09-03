bf2e45ae80ef8b8a31d3f8eeda2d7334c65d0571
selimreza777 <selimrezacse@gmail.com>
2025-09-04 02:25:21 +0600

    chain

--- chain.js
+++ chain.js
@@ -1,6 +1,16 @@
 const student = {
   name: 'rafi',
   1: 50,
+  familly: {
+      title: 'bhuiya',
+      // mother: {
+      //   name: 'noor jahan',
+      //   age: 45
+      // }
+  },
   'home-addres': 'kochu khet',
   marks: 90
 }
+// console.log(student.familly.title)
+
+console.log(student.familly.mother?.age)
