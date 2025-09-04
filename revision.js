2218d74605cf9ad1c37c2963ae8e6ae30366e65b
selimreza777 <selimrezacse@gmail.com>
2025-09-05 00:24:24 +0600

    revision

---
+++ revision.js
@@ -0,0 +1,19 @@
+/**
+ * data access:
+ * 1.dot notation
+ * 2. bracket notation
+ * 3. optional chaining
+ * -------------------
+ * array operation
+ * 1. map--> 
+ * 2. forEach -->
+ * 3. filter ---> condition on each element and get those matched the conditon match 
+ * 4. find ---> return the first element that matches the condition. if no matched item found will return null
+ * 5. (optinal) reduce
+ * ----------------
+ * 1. class
+ * 2. inheritance
+ * 3. encapsulation
+ * 4. this 
+ * 5. prototype chain
+*/ 
